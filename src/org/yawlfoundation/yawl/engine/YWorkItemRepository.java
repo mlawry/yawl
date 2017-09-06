2a9f918e656538b66173db7df83bcaef117580c5
Lawrence Yao <lawry@debian8>
2017-09-06 12:27:08 +1000

    Another temp fix to https://github.com/yawlfoundation/yawl/issues/611 Fixed compile bug.

--- src/org/yawlfoundation/yawl/engine/YWorkItemRepository.java
+++ src/org/yawlfoundation/yawl/engine/YWorkItemRepository.java
@@ -22,6 +22,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.yawlfoundation.yawl.elements.YAWLServiceGateway;
 import org.yawlfoundation.yawl.elements.YAWLServiceReference;
+import org.yawlfoundation.yawl.elements.YNet;
 import org.yawlfoundation.yawl.elements.YTask;
 import org.yawlfoundation.yawl.elements.state.YIdentifier;
 
