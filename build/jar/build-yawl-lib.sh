5957d307fc8426ce7e1b0022946eaa7277a63ab7
mlawry <mlawry@gmail.com>
2017-10-30 17:07:10 +1100

    Added and updated JARs required by resourceServices.
    install-jsf-api + install-jsf-impl
    ----------------------------------
    JARs javax.faces:jsf-api + javax.faces:jsf-impl (from central repo) and
    org.netbeans.external:jsf-api + org.netbeans.external:jsf-impl (from bits.netbeans.org/nexus)
    don't actually work (resourceService gives ArrayIndexOutOfBoundsException after sign in).
    So reverted to using the JARs from `build/3rdParty/lib`.
    
    install-jsf-appbase + install-jsf-errorhandler + install-jsf-webui
    ------------------------------------------------------------------
    Found these JARs in bits.netbeans.org/nexus maven repo, so downloaded them and set them up
    with the same group:artifact:version (user may not have set up access to NetBeans maven repo).
    
    install-jsf-webui-theme
    -----------------------
    This JAR is required by resourceService but not previously included, preventing resourceService
    from working properly at runtime. I couldn't find this JAR in any maven repo so using the JAR
    from `build/3rdParty/lib`.
    
    build-yawl-lib.sh + yawl-lib/pom.xml
    ------------------------------------
    Files updated due to JAR changes above. Also added a few additional JARs required by
    resourceService but not previously included.

--- build/jar/build-yawl-lib.sh
+++ build/jar/build-yawl-lib.sh
@@ -4,13 +4,22 @@ echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
 echo 'MUST run this shell script from `yawl/build/jar` folder.'
 echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
 
-# Cannot find the following JARs in maven repo, so have to install them locally first.
-cp ../3rdParty/lib/appbase.jar install-jsf-appbase/ && \
-cp ../3rdParty/lib/webui.jar   install-jsf-webui/   && \
-cp ../3rdParty/lib/wsif.jar    install-wsif/        && \
-mvn -f install-jsf-appbase/pom.xml package          && \
-mvn -f install-jsf-webui/pom.xml   package          && \
-mvn -f install-wsif/pom.xml        package
+# Cannot find the following JARs in maven central repo, so have to install them locally first.
+# These JARs were found in bits.netbeans.org/nexus
+mvn -f install-jsf-appbase/pom.xml        package   && \
+mvn -f install-jsf-errorhandler/pom.xml   package   && \
+mvn -f install-jsf-webui/pom.xml          package
+# Use local copy for these JARs, javax.faces:jsf-api (from central repo) and org.netbeans.external:jsf-api
+# (from bits.netbeans.org/nexus) equivalents don't actually work. Same goes for jsf-impl equivalents.
+cp ../3rdParty/lib/jsf-api.jar            install-jsf-api/           && \
+cp ../3rdParty/lib/jsf-impl.jar           install-jsf-impl/          && \
+mvn -f install-jsf-api/pom.xml            package                    && \
+mvn -f install-jsf-impl/pom.xml           package
+# Use local copy for these JARs, cannot find them in any repo.
+cp ../3rdParty/lib/wsif.jar               install-wsif/              && \
+cp ../3rdParty/lib/defaulttheme-gray.jar  install-jsf-webui-theme/   && \
+mvn -f install-wsif/pom.xml               package                    && \
+mvn -f install-jsf-webui-theme/pom.xml    package
 
 # Now we can copy over the src and build.
 # For xml and xsd files, copy them to src/main/resources using tar.
